fb373a0f940ba5a557d09cd3d8d51346b03d544d
Tecnologia Xico <saulaxios@gmail.com>
2018-09-03 17:50:54 -0500

    Respaldo 3 sep 2018

---
+++ build.js
@@ -0,0 +1,24 @@
+// C:\Users\sdkca\Desktop\electron-workspace\build.js
+var electronInstaller = require('electron-winstaller');
+
+// In this case, we can use relative paths
+var settings = {
+    // Specify the folder where the built app is located
+    appDirectory: './release-builds/CostosProduccionXico-win32-x64',
+    // Specify the existing folder where 
+    outputDirectory: './installers',
+    // The name of the Author of the app (the name of your company)
+    authors: 'Our Code World Inc.',
+    // The name of the executable of your built
+    exe: './CostosProduccionXico.exe',
+    description: 'test description',
+    setupIcon: './icon.ico'
+};
+
+resultPromise = electronInstaller.createWindowsInstaller(settings);
+ 
+resultPromise.then(() => {
+    console.log("El instalador fue creado con exito!");
+}, (e) => {
+    console.log(`No se pudo crear el instalador: ${e.message}`)
+});
